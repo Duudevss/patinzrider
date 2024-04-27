c1baed3c4a31e6331c965b25d93c4845d0deaa0d
patinz <duudevss1223@gmail.com>
2024-04-28 00:45:39 +0700

    Add path 'app_general.urls'

--- project_ptrd/urls.py
+++ project_ptrd/urls.py
@@ -18,6 +18,6 @@ from django.contrib import admin
 from django.urls import path,include
 
 urlpatterns = [
-    path('', include('project_ptrd.urls')),
+    path('', include('app_general.urls')),
     path('admin/', admin.site.urls),
 ]
